292cedc1d8a27161419abc20a53abb9442a42172
‘Karthik <karthikchandrasekhar03@outlook.com>
2024-03-27 22:13:29 +0530

    Added House Robber problem

---
+++ HouseRobber.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums):
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+        dp = [0]*n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        return dp[i]
+
+if __name__ == '__main__':
+    solution = Solution()
+    inp = list(map(int, input().split()))
+    print(solution.rob(inp))
